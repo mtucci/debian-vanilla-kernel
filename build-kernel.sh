9087aa0a4857427e4346d7890812b47214e296a5
mtucci <tucci.michele@gmail.com>
2023-02-21 11:01:02 +0100

    If no argument is passed, get the latest stable

--- build-kernel.sh
+++ build-kernel.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 
+if [[ $# -ne 1 ]]
+then
+    # If no argument is passed, get the latest stable
+    download_url=$(curl -s https://www.kernel.org | grep -A3 '>stable:<' | grep -o 'https://[^"]*')
+else
+    download_url="https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-${1}.tar.xz"
+fi
+
+# Download and unpack
 cd /build
-download_url=$(curl -s https://www.kernel.org | grep -A3 '>stable:<' | grep -o 'https://[^"]*')
 curl "$download_url" --output kernel.tar.xz
 tar xavf kernel.tar.xz
-rm kernel.tar.xz
 cd linux-[0-9\.]*
-cp $(ls -1 /boot/config-* | tail -n1) .config
+
+# Config and compile
+cp /config .config
 scripts/config --disable MODULE_SIG
 scripts/config --disable DEBUG_INFO
 yes "" | make oldconfig
 make clean
 make -j 20 deb-pkg
 cd ..
+
+# Clean up
+rm kernel.tar.xz
 rm -rf linux-[0-9\.]*
